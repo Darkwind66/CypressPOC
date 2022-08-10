1238c85076e541ef34c3618d8645e89fa20c7e05
Leandro <leandro.rodrigo_ntconsult@prestador.globo>
2022-08-10 15:55:01 -0300

    Remocao da antiga pasta cypress 9_6

--- Cypress_9_6/cypress/integration/recommendations/getRecommendation.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress"/>
-
-function getAllRecommendations(){
-    return cy.request({
-        method: 'GET',
-        url: Cypress.env('API_RECOMMENDATION_URL'),
-    })
-}
-export{getAllRecommendations}
